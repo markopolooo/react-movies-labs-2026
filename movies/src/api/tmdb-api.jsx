dc0e8a023a7cf4f2c7baed226321629f41a9ec80
John Doe <johndoe@example.com>
2026-03-24 02:02:56 +0000

    fixed pagination on home page

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -2,7 +2,7 @@ export const getMovies = ({ queryKey }) => {
     const [, pagePart] = queryKey;
   const { page } = pagePart;
   return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${page}`
   ).then((response) => {
     if (!response.ok) {
       return response.json().then((error) => {
